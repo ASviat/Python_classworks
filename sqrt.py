1e8d766552b3fb106264ded45f61531d6c741d1e
Sviatoslav <ecrprague.asi@gmail.com>
2022-12-11 22:26:38 +0300

    Done

--- sqrt.py
+++ sqrt.py
@@ -1,12 +1,12 @@
-#Напишите прогу, принимающую два числа и проверяющую, является ли одно квадратом другого.
+# Напишите прогу, принимающую два числа и проверяющую, является ли одно квадратом другого.
 
-num_first=int(input ("Введите число 1: "))
-num_second=int(input("Введите число 2: "))
+num_first = int(input("Введите число 1: "))
+num_second = int(input("Введите число 2: "))
 
-if num_first*num_first==num_second:
-    print ('Второе число является квадратом первого')
+if num_first*num_first == num_second:
+    print('Второе число является квадратом первого')
 
-elif num_second*num_second==num_first:
+elif num_second*num_second == num_first:
     print('Первое является квадратом второго')
 else:
     print('Мавр ошибся.')
